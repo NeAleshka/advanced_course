c1fc1324037beb51c4dfa9a4f87b44801e450ec2
NeAleshka <alexeybudai@gmail.com>
2023-07-31 00:20:39 +0300

    fix GitHub actions

--- src/app/providers/StoreProvider/config/StoreSchema.ts
+++ src/app/providers/StoreProvider/config/StoreSchema.ts
@@ -5,3 +5,4 @@ export interface StoreSchema {
 	user: UserSchema;
 	loginForm: LoginSchema;
 }
+// comment
--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -18,3 +18,4 @@ export const StoreProvider = (props: StoreProviderProps) => {
 		</Provider>
 	);
 };
+// comment
