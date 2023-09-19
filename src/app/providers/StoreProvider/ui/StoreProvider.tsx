a1f90c48fe3d3df0a37014b41d14b64ef4599776
NeAleshka <alexeybudai@gmail.com>
2023-09-19 22:57:57 +0300

    fix github-actions

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,21 +2,23 @@ import { Provider } from 'react-redux';
 import { createReduxStore } from 'app/providers/StoreProvider';
 import { type StoreSchema } from 'app/providers/StoreProvider/config/StoreSchema';
 import { type DeepPartial, type ReducersMapObject } from '@reduxjs/toolkit';
-import { ReactNode } from 'react';
+import React, { type ReactNode } from 'react';
 import { useNavigate } from 'react-router';
 
 interface StoreProviderProps {
-    children: ReactNode;
-    initialState?: DeepPartial<StoreSchema> ;
-	asyncReducers?: DeepPartial<ReducersMapObject<StoreSchema>>;}
+	children?: ReactNode;
+	initialState?: DeepPartial<StoreSchema> ;
+	asyncReducers?: DeepPartial<ReducersMapObject<StoreSchema>>;
+}
 
 export const StoreProvider = (props: StoreProviderProps) => {
     const { children, initialState, asyncReducers } = props;
     const navigate = useNavigate();
+
     const store = createReduxStore(
-initialState as StoreSchema,
-asyncReducers as ReducersMapObject<StoreSchema>,
-navigate,
+        initialState as StoreSchema,
+        asyncReducers as ReducersMapObject<StoreSchema>,
+        navigate,
     );
     return (
         <Provider store={store}>
